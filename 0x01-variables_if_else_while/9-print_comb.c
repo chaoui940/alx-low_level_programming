8b3cd555bfab98660641b4843ec7358aee3a3cfd
chaoui940 <daging129@gmail.com>
2023-11-18 02:58:13 -0800

    LowLvlProgramming

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - entry point to the programme
+ *
+ * Return: alwways 0 (Sucess)
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int first_number;
+
+	first_number = 48;
+	while (first_number != 58)
+	{
+		putchar(first_number);
+		if (first_number == 57)
+			break;
+		/*print comma*/
+		putchar(44);
+		/*print space*/
+		putchar(32);
+		first_number++;
+	}
+	putchar('\n');
+	return (0);
+}
